58739640e932bed69593678bc8112beb4e1850d6
Eric Hartmann <hartmaec@rh05659.villa-bosch.de>
2024-02-26 18:59:05 +0100

    make grappa v.1.1 compatible (tested)

--- tests/test_parameterizers.py
+++ tests/test_parameterizers.py
@@ -4,112 +4,16 @@ import pytest
 pytest.importorskip("grappa")
 
 import os
+import math
 from pathlib import Path
 from copy import deepcopy
-import numpy as np
-import json
-
-from grappa_interface import (
-    convert_parameters,
-    build_molecule,
-    apply_parameters,
-    GrappaInterface,
-    load_model,
-)
-
-from grappa.grappa import Grappa
-from grappa.data.Molecule import Molecule
-from grappa.data.Parameters import Parameters
 
 from kimmdy.topology.topology import Topology
-from kimmdy.constants import AA3
-from kimmdy.parsing import write_json, read_json, read_top, write_top
+from kimmdy.topology.atomic import MultipleDihedrals
+from kimmdy.parsing import read_top
 
 
-# generic functions
-def array_eq(arr1, arr2):
-    return (
-        isinstance(arr1, np.ndarray)
-        and isinstance(arr2, np.ndarray)
-        and arr1.shape == arr2.shape
-        and (arr1 == arr2).all()
-    )
-
-
-## fixtures ##
-@pytest.fixture
-def grappa_input():
-    return Molecule.from_json(Path(__file__).parent / "in_alanine.json")
-
-
-@pytest.fixture
-def grappa_output_raw():
-    ref_dict = read_json(Path(__file__).parents[0] / "out_raw_alanine.json")
-    for k, v in ref_dict.items():
-        ref_dict[k] = np.asarray(v)
-    output_raw = Parameters.from_dict(ref_dict)
-    return output_raw
-
-
-@pytest.fixture
-def grappa_output_converted():
-    ref_dict = read_json(Path(__file__).parents[0] / "out_converted_alanine.json")
-    output_converted = Parameters.from_dict(ref_dict)
-    return output_converted
-
-
-## test scripts ##
-def test_generate_input():
-    top = Topology(read_top(Path(__file__).parent / "Ala_out.top"))
-    mol = build_molecule(top)
-    out_path = Path(__file__).parents[0] / "tmp" / "in.json"
-    out_path.parents[0].mkdir(parents=True, exist_ok=True)
-    mol.to_json(out_path)
-
-    assert len(mol.atoms) == 21
-    assert len(mol.bonds) == 20
-    assert len(mol.impropers) == 15
-    feature_keys = list(mol.additional_features.keys())
-    assert all(x in feature_keys for x in ["is_radical", "ring_encoding"])
-    assert mol.atomic_numbers[8] == 6
-    assert mol.additional_features["is_radical"][8] == 1
-
-
-def test_predict_parameters(grappa_input, grappa_output_raw):
-    model = load_model()
-
-    # initialize class that handles ML part
-    grappa = Grappa(model, device="cpu")
-    parameters = grappa.predict(grappa_input)
-
-    parameters_dict = parameters.to_dict()
-    out_path = Path(__file__).parents[0] / "tmp" / "out_raw.json"
-    out_path.parents[0].mkdir(parents=True, exist_ok=True)
-    write_json(parameters_dict, out_path)
-
-    # check for equality per attribute
-    for k in grappa_output_raw.__annotations__.keys():
-        assert array_eq(getattr(grappa_output_raw, k), getattr(parameters, k))
-
-
-def test_convert_parameters(grappa_output_raw, grappa_output_converted):
-    parameters = convert_parameters(grappa_output_raw)
-
-    parameters_dict = parameters.to_dict()
-    out_path = Path(__file__).parents[0] / "tmp" / "out_converted.json"
-    out_path.parents[0].mkdir(parents=True, exist_ok=True)
-    write_json(parameters_dict, out_path)
-
-    assert parameters == grappa_output_converted
-
-
-def test_apply_parameters(grappa_output_converted):
-    top = Topology(read_top(Path(__file__).parent / "Ala_out.top"))
-    apply_parameters(top, grappa_output_converted)
-
-    out_path = Path(__file__).parents[0] / "tmp" / "out_parameterized.json"
-    out_path.parents[0].mkdir(parents=True, exist_ok=True)
-    write_top(top.to_dict(), out_path)
+from grappa_interface import GrappaInterface
 
 
 def test_parameterize_topology(tmp_path):
@@ -120,4 +24,31 @@ def test_parameterize_topology(tmp_path):
     curr_top = deepcopy(top)
     curr_top.needs_parameterization = True
     curr_top.update_parameters()
+
     assert top != curr_top
+
+    # if charge assignment gets into grappa
+    # assert len(curr_top.atoms) == 21
+    # charge_sum = 0
+    # for atom in curr_top.atoms.values():
+    #     charge_sum += int(atom.charge)
+    # assert math.isclose(charge_sum,0,rel_tol=1e-5)
+
+    assert len(curr_top.bonds) == 20
+    for bond in curr_top.bonds.values():
+        assert bond.c0 is not None
+        assert bond.c1 is not None
+
+    assert len(curr_top.angles) == 33
+    for angle in curr_top.angles.values():
+        assert angle.c0 is not None
+        assert angle.c1 is not None
+
+    assert len(curr_top.proper_dihedrals) == 34
+    for multiple_dihedral in curr_top.proper_dihedrals.values():
+        assert len(multiple_dihedral.dihedrals) in [3, 6]
+
+    assert len(curr_top.improper_dihedrals) == 15
+    for multiple_dihedral in curr_top.improper_dihedrals.values():
+        assert isinstance(multiple_dihedral, MultipleDihedrals)
+        assert len(multiple_dihedral.dihedrals) == 3
