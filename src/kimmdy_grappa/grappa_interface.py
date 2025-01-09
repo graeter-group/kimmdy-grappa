9883743d8f088426537c310c988493adb128ab35
Eric Hartmann <hartmaec@rh05659.villa-bosch.de>
2025-01-09 14:07:41 +0100

    add logging info for tag

--- src/kimmdy_grappa/grappa_interface.py
+++ src/kimmdy_grappa/grappa_interface.py
@@ -19,6 +19,7 @@ class GrappaInterface(Parameterizer):
         self, *args, grappa_tag: str = "latest", charge_model: str = "amber99", **kwargs
     ):
         super().__init__(*args, **kwargs)
+        logger.info(f"Instantiating Grappa with tag '{grappa_tag}'.")
         grappa_instance = Grappa.from_tag(grappa_tag)
         self.kimmdy_grappa_parameterizer = KimmdyGrappaParameterizer(
             grappa_instance=grappa_instance,
